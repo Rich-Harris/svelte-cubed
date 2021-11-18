5289eb2307c82fa9f8fecc3a3b564c57f34de3d9
Rich Harris <richard.a.harris@gmail.com>
2021-11-18 16:43:39 -0500

    remove Skin type

--- src/lib/types/common.d.ts
+++ src/lib/types/common.d.ts
@@ -2,9 +2,3 @@ export type EulerOrder = 'XYZ' | 'XZY' | 'YXZ' | 'YZX' | 'ZXY' | 'ZYX';
 export type Position = [number, number, number];
 export type Rotation = [number, number, number, EulerOrder?];
 export type Scale = number | [number, number, number];
-
-// TODO remove this, it doesn't really belong in the library
-export type Skin = {
-	index: import('three').BufferAttribute;
-	weight: import('three').BufferAttribute;
-};
