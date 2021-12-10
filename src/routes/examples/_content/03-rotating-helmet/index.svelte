7d3621c7cce62c0d0b35cff203840cb5bd99896e
Pascal Galle <pascal.galle@gmx.de>
2021-12-10 19:49:50 +0100

    remove unused code

--- src/routes/examples/_content/03-rotating-helmet/index.svelte
+++ src/routes/examples/_content/03-rotating-helmet/index.svelte
@@ -4,10 +4,6 @@
 	import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 	import { onMount } from 'svelte';
 
-	let width = 1;
-	let height = 1;
-	let depth = 1;
-
 	let spin = 0;
 
 	SC.onFrame(() => {
@@ -30,7 +26,7 @@
 	fog={new THREE.FogExp2('papayawhip', 0.1)}
 	shadows
 >
-	<SC.Group position={[0, -height / 2, 0]}>
+	<SC.Group position={[0, -0.5, 0]}>
 		<SC.Mesh
 			geometry={new THREE.PlaneGeometry(50, 50)}
 			material={new THREE.MeshStandardMaterial({ color: 'burlywood' })}
@@ -60,29 +56,3 @@
 	<SC.AmbientLight intensity={0.6} />
 	<SC.DirectionalLight intensity={0.6} position={[-2, 3, 2]} shadow={{ mapSize: [2048, 2048] }} />
 </SC.Canvas>
-
-<div class="controls">
-	<label><input type="range" bind:value={width} min={0.1} max={3} step={0.1} /> width</label>
-	<label><input type="range" bind:value={height} min={0.1} max={3} step={0.1} /> height</label>
-	<label><input type="range" bind:value={depth} min={0.1} max={3} step={0.1} /> depth</label>
-</div>
-
-<style>
-	.controls {
-		position: absolute;
-		left: 1em;
-		top: 1em;
-	}
-
-	label {
-		display: flex;
-		width: 60px;
-		gap: 0.5em;
-		align-items: center;
-	}
-
-	input {
-		width: 80px;
-		margin: 0;
-	}
-</style>
